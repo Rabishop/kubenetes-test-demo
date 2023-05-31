82d10827e1f7c5581b79f8ed25d2b7776dfa82c1
Rabishop <50069587+Rabishop@users.noreply.github.com>
2023-05-31 18:13:35 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./pages/assets"))))
 
-	http.Handle("/vendor/", http.StripPrefix("/vendor/", http.FileServer(http.Dir("./vendor"))))
+	http.Handle("/vendor/", http.StripPrefix("/vendor/", http.FileServer(http.Dir("./pages/vendor"))))
 
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./pages/"+r.URL.Path+".html")
+		if r.URL.Path == "" || r.URL.Path == "/" {
+			http.ServeFile(w, r, "./index.html")
+		} else {
+			http.ServeFile(w, r, "./pages/"+r.URL.Path+".html")
+		}
 	}))
 
 	// Build the Server
-
+	log.Printf("Listening on localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
