f0e57e241385dded8bb655c9b362c428c6b7c92a
Rabishop <50069587+Rabishop@users.noreply.github.com>
2023-05-31 16:56:09 +0900

    Create main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+
+	http.Handle("/vendor/", http.StripPrefix("/vendor/", http.FileServer(http.Dir("./vendor"))))
+
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./pages/"+r.URL.Path+".html")
+	}))
+
+	// Build the Server
+
+	http.ListenAndServe(":8080", nil)
+}
