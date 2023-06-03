80e4b01ca51915e063ad203ffc60d94b5152116a
Rabishop <50069587+Rabishop@users.noreply.github.com>
2023-06-03 15:58:47 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,13 +12,28 @@ func main() {
 
 	http.Handle("/vendor/", http.StripPrefix("/vendor/", http.FileServer(http.Dir("./pages/vendor"))))
 
+	// http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	if r.URL.Path == "" || r.URL.Path == "/" {
+	// 		http.ServeFile(w, r, "./index.html")
+	// 	} else {
+	// 		http.ServeFile(w, r, "./pages/"+r.URL.Path+".html")
+	// 	}
+	// }))
+
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!\n")
+	}))
+
+	http.Handle("/index", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "" || r.URL.Path == "/" {
 			http.ServeFile(w, r, "./index.html")
 		} else {
-			http.ServeFile(w, r, "./pages/"+r.URL.Path+".html")
+			http.ServeFile(w, r, "./index.html")
 		}
 	}))
+
+	// fs := http.FileServer(http.Dir("/"))
+	// http.Handle("/", fs)
 
 	// Build the Server
 	log.Printf("Listening on localhost:8080")
